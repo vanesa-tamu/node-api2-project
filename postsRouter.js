d952ea9d56d7df89ae6752c77b155e4eace93f54
vanesa-tamu <vahe5495@tamu.edu>
2020-04-08 15:38:52 -0500

    POST

--- postsRouter.js
+++ postsRouter.js
@@ -2,7 +2,26 @@ const express = require('express')
 const router = express.Router()
 const Posts = require('./data/db.js')
 
-
-
+router.post('/', (req, res) => {
+    const body = req.body
+    if(!body.title || !body.contents){
+        res.status(400).json({ error: "Please provide title and contents for the post." });
+    }
+    Posts.insert(body)
+        .then(post => {
+            Posts.findById(post.id)
+                .then(result => {
+                    res.status(201).json(result[0])
+                })
+                .catch(err => {
+                    console.error('Err in POST', err)
+                    res.status(500).json({ error: "Err getting new post"})
+                })
+        })
+        .catch(err => {
+            console.error('Err in POST', err)
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        })
+})
 
 module.exports = router;
