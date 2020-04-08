92f457ac9537f9f19a7db18dd5b30bacdaa0801e
vanesa-tamu <vahe5495@tamu.edu>
2020-04-08 16:05:36 -0500

    GET all posts

--- postsRouter.js
+++ postsRouter.js
@@ -53,4 +53,10 @@ router.post('/:id/comments', async (req,res) => {
 
 })
 
+router.get('/', (req,res) => {
+    Posts.find()
+        .then((posts) => res.status(200).json(posts))
+        .catch(err => { res.status(500).json({error: "The posts information could not be retrieved."})})
+})
+
 module.exports = router;
