7ea2138747d2a66a3902f496ac088f2e9eea6c0a
vanesa-tamu <vahe5495@tamu.edu>
2020-04-08 16:02:41 -0500

    POST comment

--- postsRouter.js
+++ postsRouter.js
@@ -5,7 +5,7 @@ const Posts = require('./data/db.js')
 router.post('/', (req, res) => {
     const body = req.body
     if(!body.title || !body.contents){
-        res.status(400).json({ error: "Please provide title and contents for the post." });
+        res.status(400).json({ message: "Please provide title and contents for the post." });
     }
     Posts.insert(body)
         .then(post => {
@@ -22,6 +22,35 @@ router.post('/', (req, res) => {
             console.error('Err in POST', err)
             res.status(500).json({ error: "There was an error while saving the post to the database" });
         })
+})
+
+router.post('/:id/comments', async (req,res) => {
+    const { id } = req.params
+    const { post_id, text } = req.body
+
+    const post = await Posts.findById(id).first()
+    if(post){
+        console.log(post, 'posted')
+        if(!post_id || !text){
+            res.status(400).json({message: `Please provide text for the comment.`})
+        }
+        else{
+            Posts.insertComment(req.body)
+            .then(cId => {
+                console.log('comment ID', cId.id)
+                Posts.findCommentById(cId.id)
+                    .then(newComment => {
+                        res.status(201).json(newComment[0])
+                    })
+                    .catch(err => { res.status(500).json({error: "Error getting new comment"})})
+            })
+            .catch(err => { res.status(500).json({error: "Error posting comment"})})
+        }
+    }
+    else{
+        res.status(401).json({ message: `The post with the specified ID, ${id}, does not exist.` })
+    }
+
 })
 
 module.exports = router;
